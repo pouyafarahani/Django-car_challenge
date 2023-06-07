1e2e4e5cf299cb52ccd19cb0b9edda933723c5ab
pouyafarahani <105992449+pouyafarahani@users.noreply.github.com>
2023-06-07 15:26:16 +0330

    add file apps and add app car

---
+++ apps/car/models/models_car.py
@@ -0,0 +1,9 @@
+# from django.db import models
+#
+#
+# class Car(models.Model):
+#     name = models.CharField(max_length=255)
+#     cylinders = models.IntegerField()
+#     passengers = models.IntegerField()
+#     color = models.CharField(max_length=255)
+#     engine_size = models.FloatField()
